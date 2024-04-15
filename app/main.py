425be760da864e6b762f658d8589dc7f00e00507
Ebredvick <Emmbredvick@gmail.com>
2024-04-15 11:35:04 +0200

    add basic jinja template and  blank css for index page

--- app/main.py
+++ app/main.py
@@ -1,9 +1,16 @@
 import json
+from datetime import datetime
 
 import httpx
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates/")
 
 def get_api_key():
     with open('./secrets/api_key.txt') as f:
@@ -11,8 +18,14 @@ def get_api_key():
     return api_key
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", 
+                                      {"request": request, 
+                                       "title": "My Portfolio", 
+                                       "name": "My Name", 
+                                       "current_year": datetime.now().year, 
+                                       "github_url": "https://github.com/Ebredvick", 
+                                       "linkedin_url": "https://www.linkedin.com/in/ethan-bredvick-967b52123"})
 
 @app.get("/github/{user}/repos")
 async def get_github_repos(user: str):
@@ -26,4 +39,3 @@ async def get_github_repos(user: str):
         response = await client.get(f"https://api.github.com/users/{user}/repos", 
                                     headers=headers)
     return response.json()
-
