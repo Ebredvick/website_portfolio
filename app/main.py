89c2a5dd44ab81a8f059b3edbfa22a2b49d67fc6
Ebredvick <Emmbredvick@gmail.com>
2024-04-15 10:56:53 +0200

    add get_github_repos api

--- app/main.py
+++ app/main.py
@@ -1,8 +1,29 @@
+import json
+
+import httpx
 from fastapi import FastAPI
 
 app = FastAPI()
 
+def get_api_key():
+    with open('./secrets/api_key.txt') as f:
+        api_key = f.read()
+    return api_key
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/github/{user}/repos")
+async def get_github_repos(user: str):
+    api_key = get_api_key() 
+    async with httpx.AsyncClient() as client:
+        headers = {
+            'Authorization': f'{api_key}', 
+            'Accept': 'application/vnd.github.v3+json'
+            }
+        print(headers)
+        response = await client.get(f"https://api.github.com/users/{user}/repos", 
+                                    headers=headers)
+    return response.json()
+
